80c9a875dd082f962cda3649cec68f6f9562d75d
Manica Vlad Bogdan <bugy_m11@yahoo.com>
2013-06-22 04:54:58 -0400

    Random Array

--- src/com/example/memorygame/MainActivity.java
+++ src/com/example/memorygame/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.memorygame;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,13 +13,32 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i = 0; i < 12;)
+		{
+			int rand = ((int)(Math.random() * 12)) + 1;
+			
+			if(!list.contains(rand))
+	    	{
+	    	list.add(rand);
+	    	i++;
+	    	}
+		}
+		System.out.println("dksam");
+		
+		int arraySize = list.size();
+		for(int i = 0; i < arraySize; i++) {
+			Log.d("TAG", "" + list.get(i)+ " ");
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		return true;		
+		
 	}
 
 }
